c8d8c105bd7d9e8bb7140c79b79a2e7eef48ceea
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-07-25 06:56:57 +0000

    datatypes

---
+++ Js-basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=234
+let accountEmail="abc@gmail.com"
+var accountPassword="345543"
+accountCity="Delhi"
+let accountState
+
+// accountId=986
+// console.log(accountId);
+
+/*
+prefer not to use var
+bcs of issues in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ Js-basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)  //we are using node js not browser here so it will not work 
+console.log(3
+    
+    +3) //always maintain code readability....it should be high
+    console.log("Abhishek");
+    let name=null
+    let city
+    console.log([name,city]);
+    console.log(typeof name);   //object
+    console.log(typeof city);   //undefined
+
+
+    //number=> 2 to power 53 or 52
+    //bigint=> number is large
+    //string=>""
+    //boolean=>true/false
+    //null=>standlone value
+    //symbol=>majorly used in react where component uniqueness is must identified.
