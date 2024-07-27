2b99e4c9af3f6e302769cd20c6140c83a69f0cf5
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-07-27 06:18:09 +0000

    conversion of data types

---
+++ Js-basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "20"
+// console.log(typeof (score))
+
+let scoreinnumber = Number(score)
+// console.log(typeof (scoreinnumber))
+
+// "20"=>20(number)
+// "20abc"=>NaN(number)   Not a number
+// true=>1
+
+
+let isLoggedIn = 1
+let booleanvalue = Boolean(isLoggedIn)
+console.log(booleanvalue);
