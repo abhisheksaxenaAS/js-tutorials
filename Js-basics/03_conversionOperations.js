dc1f1d3fbb32e3f8a2baeb606b131f96e615fc7a
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-08-09 04:40:22 +0000

    Conversion

--- Js-basics/03_conversionOperations.js
+++ Js-basics/03_conversionOperations.js
@@ -1,9 +1,9 @@
 let score = "20abc" //"20", nulll, undefined
-console.log(typeof (score))
+// console.log(typeof (score))
 
 let scoreinnumber = Number(score)
-console.log(typeof (scoreinnumber))
-console.log(scoreinnumber);
+// console.log(typeof (scoreinnumber))
+// console.log(scoreinnumber);
 
 
 // "20"=>20(number)
@@ -13,4 +13,18 @@ console.log(scoreinnumber);
 
 let isLoggedIn = 1
 let booleanvalue = Boolean(isLoggedIn)
-console.log(booleanvalue);
+// console.log(booleanvalue);
+// console.log(typeof booleanvalue);
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+let str1= "Hello"
+let str2= " Abhishek"
+let str3= str1+str2
+//console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);   //*
+console.log(1+2+"2");   //*
