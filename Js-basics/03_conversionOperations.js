f7bb952ea0938339700c6401f1e50a250ad62a0c
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-08-09 05:10:27 +0000

    done

--- Js-basics/03_conversionOperations.js
+++ Js-basics/03_conversionOperations.js
@@ -15,16 +15,32 @@ let isLoggedIn = 1
 let booleanvalue = Boolean(isLoggedIn)
 // console.log(booleanvalue);
 // console.log(typeof booleanvalue);
-let value =3
+let value = 3
 let negValue = -value
 //console.log(negValue);
 
-let str1= "Hello"
-let str2= " Abhishek"
-let str3= str1+str2
+let str1 = "Hello"
+let str2 = " Abhishek"
+let str3 = str1 + str2
 //console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2+2);   //*
-console.log(1+2+"2");   //*
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);   //*
+// console.log(1 + 2 + "2");   //*
+// console.log(+true);
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let counter = 100;
+// counter++;
+++counter;
+console.log(counter);
+
+let st1 = 30;
+// let st2= st1++;
+let st2 = ++st1;
+console.log(st1, st2);
+
