a37b033704f1e7ab363853fe2e04989c95d0f38f
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-08-13 09:23:22 +0000

    datatype summary

---
+++ Js-basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+
+//Primitive
+
+//7 types:String,Number,Boolen,null,undefined, Symbol,BigInt
+
+
+
+const score = 100
+const scoreValue= 100.23
+const BigNumber=2343432553253532532533532n
+
+const isLoggedin = false
+const outsideTemp =null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+// Reference(Non-Primitive)
+
+//Array,Objects,Functions 
+
+const heros=["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name:"Abhishek",
+    age: 21,
+}
+
+const myFunction= function(){
+    console.log("Hello Duniya");
+}
+
+console.log(typeof BigNumber);
+
