1a49a8ee4d769c6c717ba45357b8f1239f5c3d2d
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-08-16 10:46:40 +0000

    videoend

---
+++ Js-basics/06_numsAndmaths.js
@@ -0,0 +1,34 @@
+const Score= 300
+//console.log(Score);
+
+const Balance = new Number(500)
+//console.log(Balance);
+
+//console.log(Balance.toString().length);
+//console.log(Balance.toFixed(2));
+
+
+const otherNumber = 551.734474
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));      //*
+
+//++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.7));
+// console.log(Math.floor(323.9));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,3,4,5,6));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) +1);
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+
+
