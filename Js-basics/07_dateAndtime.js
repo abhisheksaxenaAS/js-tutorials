9b46830ddccd9ad6ba88d90978da621c01dff066
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-08-17 03:56:30 +0000

    break

---
+++ Js-basics/07_dateAndtime.js
@@ -0,0 +1,38 @@
+const myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+
+
+let myCreatedDate= new Date(2024,7,17)
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate1= new Date("2024-08-17")
+// console.log(myCreatedDate1.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp.toLocaleString());
+// console.log(myCreatedDate1.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth())
+console.log(newDate.getDay());
+
+
+
+
+
+
+
+
