b40c3d1467dbfa1aa54fa93bda3b28d02cb2ddd6
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-10-16 13:30:21 +0000

    video completed

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -10,8 +10,7 @@ function MyName() {
     console.log("E");
     console.log("K");
 }
-
-// MyName()
+//MyName()
 
 // function addtwoNumbers(num1,num2){
 //     console.log(num1+num2);
@@ -19,6 +18,27 @@ function MyName() {
 // addtwoNumbers(5,"7")
 
 function addtwoNumbers(num1, num2) {
-    let result = num1 + num2
-
+    // let result = num1 + num2
+    // return result
+    return num1 + num2
 }
+const result = addtwoNumbers(2, 1)
+//console.log("Result is", result)
+
+function loginUsermessage(username) {
+    // if(username===undefined){
+    //     console.log("Please enter a valid username");
+    //     return username
+    //     }
+    if (!username) {
+        console.log("Please enter a valid username");
+        return username
+    }
+    return `${username} just logged in.`
+}
+// const message = loginUsermessage("Abhishek")
+// console.log(message);
+
+// console.log(loginUsermessage("Abhishek"))
+
+console.log(loginUsermessage());
