263c7ffcfddde0fa9c75d3156eecdf4d9fb4efd2
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-10-16 08:18:39 +0000

    will continue

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,24 @@
+
+
+function MyName() {
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("E");
+    console.log("K");
+}
+
+// MyName()
+
+// function addtwoNumbers(num1,num2){
+//     console.log(num1+num2);
+// }
+// addtwoNumbers(5,"7")
+
+function addtwoNumbers(num1, num2) {
+    let result = num1 + num2
+
+}
