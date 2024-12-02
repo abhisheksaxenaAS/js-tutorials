9f17fe926cf86f05c8e2098af4f01d73b8a53974
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-12-02 06:06:36 +0000

    1stHalf

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,31 @@
+//if
+const isUserLoggedIn = true
+// if (2 === "2") {
+//     console.log("Condtion True, Code executed");
+// }
+// else {
+//     console.log("Condition false.");
+
+// }
+
+
+// const temp = 40
+// if (temp < 50) {
+//     console.log("Temprature is less than 50");
+// }
+// // else {
+// //     console.log("Temperature is higher than 50");
+
+// // }
+// console.log("Temperature is higher than 50");
+
+
+// const score = 200
+// if (score > 100) {
+//     const power = "fly"
+//     // let power = "fly"
+//     console.log(`user power: ${power}`);
+
+// }
+// console.log(`user power: ${power}`);
+
