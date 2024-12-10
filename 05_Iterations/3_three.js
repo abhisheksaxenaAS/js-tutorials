e3307226f78c78379d48248c0f30f5c425eb5e93
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-12-10 09:40:12 +0000

    name changed

---
+++ 05_Iterations/3_three.js
@@ -0,0 +1,16 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+// const arr = [1,2,3,4,5,6]
+// for (const num of arr) {
+//     console.log(num);
+
+// }
+
+const greeting = "Hello World!"
+for (const greets of greeting) {
+    console.log(`Each char in ${greets}`);
+
+}
