049b4f931ebabf2378ebf6859c2ea97e552f004f
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-12-11 05:38:06 +0000

    1st half

---
+++ 05_Iterations/6_six.js
@@ -0,0 +1,43 @@
+// const coding = ["js", "rb", "python", "java"]
+// // coding.forEach((item) => {
+// //     console.log(item);
+
+// // })
+
+
+
+// // const values = coding.forEach((item) => {
+// //     console.log(item);
+// // return item
+// // })
+// // console.log(values); //>>>>>undefined
+
+
+
+// const myNums = [1,2,3,4,5,6,7,8]
+// const newNums = myNums.filter(  (num)=> num>4)
+// console.log(newNums);
+
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8]
+// const newNums = myNums.filter((num) => {
+//     return num > 4
+// })
+// console.log(newNums);
+
+
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8]
+// const newNums = []
+// myNums.forEach((num)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
+//11.59
+
+
