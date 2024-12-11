50451a1d28d77f76fd9f299016227808df445660
Abhishek Saxena <117523216+abhisheksaxenaAS@users.noreply.github.com>
2024-12-11 04:37:27 +0000

    3rd done

--- 05_Iterations/3_three.js
+++ 05_Iterations/3_three.js
@@ -11,6 +11,32 @@
 
 const greeting = "Hello World!"
 for (const greets of greeting) {
-    console.log(`Each char in ${greets}`);
+    //console.log(`Each char in ${greets}`);
 
 }
+
+
+// //maps
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "Unied States of America")
+// map.set('FR', "France")
+// map.set('FR', "France")
+// map.set('WI', "West Indies")
+// //console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+
+// }
+
+
+// const myObject = {
+//     'game1': "Contra",
+//     'game2': "BGMI"
+// }
+// for (const [keys, value] of myObject) {
+//     console.log(keys, ':-', value);
+
+// }
+
